0b3fd2f016a25b906149b87c7accccccc2d8eb18
pywoo <kfdsy0103@naver.com>
2025-11-02 21:01:45 +0900

    feat(LottoMachine): 입력값 재시도 로직 구현
    구입금액, 당첨번호, 보너스번호를 입력받을때, 예외발생 시 메시지 처리 및 재시도 로직을 작성하였습니다.

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,10 +1,68 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoMachine {
 
     public static final int LOTTO_PRICE = 1000;
 
     public void play() {
+        int purchasePrice = getPurchasePrice();
+        int purchaseCount = purchasePrice / LOTTO_PRICE;
 
+        List<Lotto> lottos = generateLottos(purchaseCount);
+
+        List<Integer> winningNumber = getWinningNumber();
+        Lotto winningLotto = new Lotto(winningNumber);
+        int bonusNumber = getBonusNumber();
     }
+
+    private List<Lotto> generateLottos(int count) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto(generateLottoNumbers()));
+        }
+        ConsoleIO.printPurchaseInfo(lottos);
+        return lottos;
+    }
+
+    private List<Integer> generateLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    private int getPurchasePrice() {
+        while (true) {
+            try {
+                String purchasePriceInput = ConsoleIO.inputPurchasePrice();
+                return LottoParser.parsePurchasePrice(purchasePriceInput);
+            } catch (Exception e) {
+                ConsoleIO.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private List<Integer> getWinningNumber() {
+        while (true) {
+            try {
+                String winningNumberInput = ConsoleIO.inputWinningNumber();
+                return LottoParser.parseWinningNumber(winningNumberInput);
+            } catch (Exception e) {
+                ConsoleIO.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private int getBonusNumber() {
+        while (true) {
+            try {
+                String bonusNumberInput = ConsoleIO.inputBonusNumber();
+                return LottoParser.parseBonusNumber(bonusNumberInput);
+            } catch (Exception e) {
+                ConsoleIO.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
 }
