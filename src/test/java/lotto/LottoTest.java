87f449fae248b7d2eab76e1f1e5bf90c93d4e2d8
pywoo <kfdsy0103@naver.com>
2025-11-02 21:39:53 +0900

    test(Lotto): 테스트 코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,11 @@
 package lotto;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
     @Test
@@ -21,5 +21,17 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("두 로또 간에 일치하는 숫자의 개수를 올바르게 계산한다.")
+    @Test
+    void 로또_번호_일치_개수_계산() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto winningLotto = new Lotto(List.of(4, 5, 6, 7, 8, 9));
+
+        // when
+        int matchedCount = lotto.countMatchedNumber(winningLotto);
+
+        // then
+        assertThat(matchedCount).isEqualTo(3);
+    }
 }
