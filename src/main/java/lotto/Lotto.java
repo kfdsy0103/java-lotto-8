2f2a3d288c475301f8ecc50d7da93d5e8c3a674a
pywoo <kfdsy0103@naver.com>
2025-11-02 21:04:52 +0900

    feat(Lotto): 일치하는 번호 개수 반환기능 구현
    Lotto 클래스에 countMatchedNumber(Lotto)를 작성하여, 몇 개의 수가 일치하는지 처리하는 역할을 부여하였습니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -18,5 +20,16 @@ public class Lotto {
 
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    public int countMatchedNumber(Lotto lotto) {
+        Set<Integer> otherLotto = new HashSet<>(lotto.getNumbers());
+        int count = 0;
+        for (Integer number : numbers) {
+            if (otherLotto.contains(number)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
