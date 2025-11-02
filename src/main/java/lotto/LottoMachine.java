70fc79409e7a209d1a087f251b5a6ee2f1727204
pywoo <kfdsy0103@naver.com>
2025-11-02 21:05:54 +0900

    feat(LottoMachine): 통계 기능 구현
    Map 자료구조에 당첨 정보 별 당첨 횟수를 기록하고, 수익률을 계산하여 출력하도록 하였습니다.

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -3,6 +3,8 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class LottoMachine {
 
@@ -17,6 +19,52 @@ public class LottoMachine {
         List<Integer> winningNumber = getWinningNumber();
         Lotto winningLotto = new Lotto(winningNumber);
         int bonusNumber = getBonusNumber();
+
+        Map<LottoPrize, Integer> statistic = getStatistic(lottos, winningLotto, bonusNumber);
+        double percentage = getPercentage(statistic, purchasePrice);
+
+        ConsoleIO.printResult(statistic, percentage);
+    }
+
+    private Map<LottoPrize, Integer> getStatistic(List<Lotto> lottos, Lotto winningLotto, int bonusNumber) {
+        // 일치 개수 확인
+        List<LottoPrize> matchedCount = lottos.stream()
+                .filter(lotto -> winningLotto.countMatchedNumber(lotto) >= 3)
+                .map(lotto -> {
+                    int count = winningLotto.countMatchedNumber(lotto);
+                    if (count == 5 && lotto.getNumbers().contains(bonusNumber)) {
+                        return LottoPrize.FIVE_BONUS_MATCHES;
+                    }
+                    return LottoPrize.getLottoPrizeByMatchCount(count).get();
+                })
+                .toList();
+
+        // 통계 내기
+        return getMatchCountPerLottoPrize(matchedCount);
+    }
+
+    private Map<LottoPrize, Integer> getMatchCountPerLottoPrize(List<LottoPrize> matchedCount) {
+        Map<LottoPrize, Integer> statistic = Map.of(
+                LottoPrize.THREE_MATCHES, 0,
+                LottoPrize.FOUR_MATCHES, 0,
+                LottoPrize.FIVE_MATCHES, 0,
+                LottoPrize.FIVE_BONUS_MATCHES, 0,
+                LottoPrize.SIX_MATCHES, 0
+        );
+        for (LottoPrize lottoPrize : matchedCount) {
+            statistic.put(lottoPrize, statistic.get(lottoPrize) + 1);
+        }
+        return statistic;
+    }
+
+    private double getPercentage(Map<LottoPrize, Integer> statistic, int purchasePrice) {
+        int sum = 0;
+        for (Entry<LottoPrize, Integer> entry : statistic.entrySet()) {
+            LottoPrize lottoPrize = entry.getKey();
+            Integer count = entry.getValue();
+            sum += lottoPrize.getMoney() * count;
+        }
+        return sum;
     }
 
     private List<Lotto> generateLottos(int count) {
