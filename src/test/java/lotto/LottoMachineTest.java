acb44d1363eff5c71c9985c7799fadf5545a2017
pywoo <kfdsy0103@naver.com>
2025-11-02 22:08:20 +0900

    test(LottoMachine): 통합 테스트 코드 작성

---
+++ src/test/java/lotto/LottoMachineTest.java
@@ -0,0 +1,52 @@
+package lotto;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoMachineTest extends NsTest {
+
+    @Test
+    @DisplayName("8000원을 투입하여 2등과 3등을 한 경우 수익률과 함께 성공적으로 출력한다.")
+    void 로또_성공_테스트() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("8000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "8개를 구매했습니다.",
+                            "[1, 2, 3, 4, 5, 7]",   // 2등
+                            "[1, 2, 3, 4, 5, 10]",  // 3등
+                            "[8, 21, 23, 41, 42, 43]",
+                            "[3, 5, 11, 16, 32, 38]",
+                            "[7, 11, 16, 35, 36, 44]",
+                            "[1, 8, 11, 31, 41, 42]",
+                            "[13, 14, 16, 38, 42, 45]",
+                            "[2, 13, 22, 32, 38, 45]",
+                            "3개 일치 (5,000원) - 0개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 1개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 393,650.0%입니다."
+                    );
+                },
+                List.of(1, 2, 3, 4, 5, 7),
+                List.of(1, 2, 3, 4, 5, 10),
+                List.of(8, 21, 23, 41, 42, 43),
+                List.of(3, 5, 11, 16, 32, 38),
+                List.of(7, 11, 16, 35, 36, 44),
+                List.of(1, 8, 11, 31, 41, 42),
+                List.of(13, 14, 16, 38, 42, 45),
+                List.of(2, 13, 22, 32, 38, 45)
+        );
+    }
+
+    @Override
+    public void runMain() {
+        new LottoMachine().play();
+    }
+}
