67635f485d1f1a8df0c3ae38f553b2b92615d6ed
pywoo <kfdsy0103@naver.com>
2025-11-02 19:23:52 +0900

    feat(LottoPrize): 로또 당첨 금액 Enum 구현
    일치하는 개수, 그때의 상금을 Enum으로 상수화하여 구현하였습니다.

---
+++ src/main/java/lotto/LottoPrize.java
@@ -0,0 +1,25 @@
+package lotto;
+
+public enum LottoPrize {
+    THREE_MATCHES(3, 5000),
+    FOUR_MATCHES(4, 50000),
+    FIVE_MATCHES(5, 1500000),
+    FIVE_BONUS_MATCHES(5, 30000000),
+    SIX_MATCHES(6, 2000000000);
+
+    private final int matchCount;
+    private final int money;
+
+    LottoPrize(int matchCount, int money) {
+        this.matchCount = matchCount;
+        this.money = money;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+}
