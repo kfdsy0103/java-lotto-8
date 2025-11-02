0c0552698ae9a3fd234b7fc5514db16dd0e6fff3
pywoo <kfdsy0103@naver.com>
2025-11-02 17:21:55 +0900

    feat(ConsoleIO): 입출력 기능 구현
    기능 목록에 정리한 사항을 메서드로 구현하였습니다.
    입력 : 구입 금액, 당첨 번호, 보너스 번호를 입력받습니다.
    출력 : 구매한 로또 번호 목록, 당첨 통계를 출력합니다. private 출력 메서드를 내부적으로 사용합니다.

---
+++ src/main/java/lotto/ConsoleIO.java
@@ -0,0 +1,58 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class ConsoleIO {
+
+    public static String inputPurchasePrice() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static void printPurchaseInfo(List<Lotto> lottos) {
+        System.out.println();
+        int size = lottos.size();
+        System.out.println(size + "개를 구매했습니다.");
+        lottos.forEach(ConsoleIO::printLottoInfo);
+    }
+
+    public static String inputWinningNumber() {
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static String inputBonusNumber() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public static void printResult(Map<LottoPrize, Integer> result, Double percentage) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        result.entrySet().forEach(ConsoleIO::printStatistic);
+        System.out.printf("총 수익률은 %.2f%%입니다.\n", percentage);
+    }
+
+    private static void printLottoInfo(Lotto lotto) {
+        System.out.println(lotto.toString());
+    }
+
+    private static void printStatistic(Entry<LottoPrize, Integer> statistics) {
+        LottoPrize lottoPrize = statistics.getKey();
+        Integer matchCountResult = statistics.getValue();
+
+        if (lottoPrize.equals(LottoPrize.FIVE_BONUS_MATCHES)) {
+            System.out.printf("%d개 일치, 보너스 볼 일치 (%,d원) - %d개\n", lottoPrize.getMatchCount(), lottoPrize.getMoney(),
+                    matchCountResult);
+            return;
+        }
+        System.out.printf("%d개 일치 (%,d원) - %d개\n", lottoPrize.getMatchCount(), lottoPrize.getMoney(),
+                matchCountResult);
+    }
+}
